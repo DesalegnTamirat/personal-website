3ed6b2e4331ea2feb2d763aa6dd7a01e94f1368e
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2024-10-25 19:40:53 +0300

    making the contact links functional

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,39 +2,44 @@ const Contact = () => {
   return (
     <section id="contact-section">
       <h2>My Contacts</h2>
-      <hr className="small"/>
+      <hr className="small" />
       <div className="contact-details">
         <div className="contact">
-          <div className="icon"><ion-icon name="call-outline"></ion-icon></div>
+          <div className="icon">
+            <ion-icon name="call-outline"></ion-icon>
+          </div>
           <h3>Telephone</h3>
-          <a href="#">
+          <a href="tel:+251979575672">
             Call me
           </a>
         </div>
+
         <div className="contact">
           <div className="icon">
-          <ion-icon name="mail-outline"></ion-icon>
+            <ion-icon name="mail-outline"></ion-icon>
           </div>
           <h3>Email</h3>
-          <a href="#">
+          <a href="mailto:desalegntamirateshetu@gmail.com">
             Email me
           </a>
         </div>
+
         <div className="contact">
           <div className="icon">
-          <ion-icon name="logo-github"></ion-icon>
+            <ion-icon name="logo-github"></ion-icon>
           </div>
-          <h3>Git</h3>
-          <a href="#">
+          <h3>GitHub</h3>
+          <a href="https://github.com/DesalegnTamirat" target="_blank" rel="noopener noreferrer">
             Git me
           </a>
         </div>
+
         <div className="contact">
           <div className="icon">
-          <ion-icon name="logo-linkedin"></ion-icon>
+            <ion-icon name="logo-linkedin"></ion-icon>
           </div>
-          <h3>Linkdin</h3>
-          <a href="#">
+          <h3>LinkedIn</h3>
+          <a href="https://www.linkedin.com/in/desalegn-tamirat-19727520a/" target="_blank" rel="noopener noreferrer">
             Link me
           </a>
         </div>
